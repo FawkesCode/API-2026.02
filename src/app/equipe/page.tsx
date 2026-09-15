3377374e1d79e0ead028f2f2bb0dcea130c373c0
James N. <129893041+Pottassiuw@users.noreply.github.com>
2026-09-14 23:53:50 -0300

    .

--- src/app/equipe/page.tsx
+++ src/app/equipe/page.tsx
@@ -6,6 +6,8 @@ export default function Team() {
     <h2>Página de Minha Equipe</h2>
     <PriorityButton priority="low" selected={true}>Salve</PriorityButton>
     <PriorityBadge priority="medium">cso</PriorityBadge>
+
+    <h1>We are using n8n babyyyyy!</h1>
   </>
   
 
